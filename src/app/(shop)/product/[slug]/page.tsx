1b545968483941832a86e3de7991613f76360e97
gabriel\gabi <fabrogabrielandres@gmail.com>
2024-07-12 10:50:50 +0200

    mock prodcut page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,18 +1,54 @@
-import type { Metadata } from 'next'
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+
 interface Props {
-    params: {
-        slug: string
-    }
+  params: {
+    slug: string;
+  };
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    return { title: params.slug }
+  return { title: params.slug };
 }
 
 export default function ProductPage({ params }: Props) {
-    return (
-        <div>
-            <h1>{params.slug}</h1>
-        </div>
-    );
+  const { slug } = params;
+
+  const product = initialData.products.find(
+    (product) => product.slug === slug
+  );
+
+  if (!product) {
+    notFound();
+  }
+
+  return (
+    <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+      {/* Slideshow */}
+      <div className="col-span-1 md:col-span-2 ">
+        {/* Mobile Slideshow */}
+
+
+        {/* Desktop Slideshow */}
+      </div>
+
+      {/* Detalles */}
+      <div className="col-span-1 px-5">
+        {/* SizeSelector */}
+
+        <h1 className={` ${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+
+        <p className="text-lg mb-5">${product.price}</p>
+
+
+        {/* Descripción */}
+        <h3 className="font-bold text-sm">Descripción</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
+    </div>
+  );
 }
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
+export * from "./Product/Size-selector/SizeSelector";
 export * from "./ui/Sidebar/Sidebar";
 export * from "./Title/Title";
 export * from "./ui/not-found/PageNotFound";
