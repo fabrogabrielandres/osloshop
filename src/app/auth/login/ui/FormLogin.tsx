f91fceb3f7e49281ae6fc1151f4741fae22153b3
gabriel <gabreil>
2024-08-30 13:02:15 +0200

    start with auth config

---
+++ src/app/auth/login/ui/FormLogin.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import React from "react";
+
+export const FormLogin = () => {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor="email">Correo electrónico</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="email"
+      />
+
+      <label htmlFor="email">Contraseña</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="email"
+      />
+
+      <button className="btn-primary">Ingresar</button>
+
+      {/* divisor l ine */}
+      <div className="flex items-center my-5">
+        <div className="flex-1 border-t border-gray-500"></div>
+        <div className="px-2 text-gray-800">O</div>
+        <div className="flex-1 border-t border-gray-500"></div>
+      </div>
+
+      <Link href="/auth/new-account" className="btn-secondary text-center">
+        Crear una nueva cuenta
+      </Link>
+    </div>
+  );
+};
