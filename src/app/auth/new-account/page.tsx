1c904df44003d0e06b01372850219662df511d21
gabriel <gabreil>
2024-09-13 15:27:25 +0200

    form new account

--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,7 +1,31 @@
+"use client";
 import { titleFont } from "@/config/fonts";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import Link from "next/link";
+import clsx from "clsx";
 
 export default function NewAccountPage() {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      password: "",
+      email: "",
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      password: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .min(6, "Must be 6 characters or more")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
       <h1 className={`${titleFont.className} text-4xl mb-5`}>Nueva cuenta</h1>
@@ -9,23 +33,77 @@ export default function NewAccountPage() {
       <div className="flex flex-col">
         <label htmlFor="email">Nombre completo</label>
         <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          className={clsx("px-5 py-2 border bg-gray-200 rounded", {
+            "border-red-500": formik.errors.firstName,
+          })}
+          id="firstName"
+          name="firstName"
           type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.firstName}
         />
+        <span className="text-red-500 mb-5">
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <div>{formik.errors.firstName}</div>
+          ) : null}
+        </span>
 
         <label htmlFor="email">Correo electrónico</label>
         <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          className={clsx("px-5 py-2 border bg-gray-200 rounded", {
+            "border-red-500": formik.errors.email,
+          })}
+          id="email"
+          name="email"
           type="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
         />
+        <span className="text-red-500 mb-5">
+          {formik.touched.email && formik.errors.email ? (
+            <div>{formik.errors.email}</div>
+          ) : null}
+        </span>
 
         <label htmlFor="email">Contraseña</label>
         <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="email"
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+          className={clsx("px-5 py-2 border bg-gray-200 rounded", {
+            "border-red-500": formik.errors.password,
+          })}
         />
+        <span className="text-red-500 mb-5">
+          {formik.touched.password && formik.errors.password ? (
+            <div>{formik.errors.password}</div>
+          ) : null}
+        </span>
 
-        <button className="btn-primary">Crear cuenta</button>
+        <button
+          className="btn-primary"
+          disabled={
+            !!formik.errors.email ||
+            !!formik.errors.firstName ||
+            !!formik.errors.password
+          }
+        >
+          Crear cuenta
+        </button>
+
+        <span>
+          Es disable{" "}
+          {JSON.stringify(
+            !!formik.errors.email ||
+              !!formik.errors.firstName ||
+              !!formik.errors.password
+          )}
+        </span>
 
         {/* divisor l ine */}
         <div className="flex items-center my-5">
