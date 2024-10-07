711a1efa02d73b9b2c342ab1045c19238081353e
gabriel <gabreil>
2024-10-07 10:56:16 +0200

    add valid sizes to seeds

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,29 +1,27 @@
-import { initialData } from "./seed";
+import { initialData, ValidSizes } from "./seed";
 import prisma from "../lib/prisma";
 import { countriesSeed } from "./seed-countrys";
 
 async function Main() {
   if (process.env.NODE_ENV === "production") return;
 
-  const { categories,products,user } = initialData
-  
-  await prisma.addressUser.deleteMany();
+  const { categories, products, user } = initialData;
 
+  await prisma.addressUser.deleteMany();
 
   //delete all users previus
   await prisma.user.deleteMany();
-  
+
   //delete all registers previus
   await prisma.procutImage.deleteMany();
   await prisma.producStock.deleteMany();
 
-
   await prisma.product.deleteMany();
   await prisma.category.deleteMany();
   await prisma.countrys.deleteMany();
-  
+
   //insert Users
-  await prisma.user.createMany({data:initialData.user})
+  await prisma.user.createMany({ data: initialData.user });
 
   //insert categories
   const dataToCreateCategoriInDb = categories.map((category) => {
@@ -31,9 +29,9 @@ async function Main() {
   });
 
   await prisma.category.createMany({ data: dataToCreateCategoriInDb });
-  
+
   const categoriesFromDB = await prisma.category.findMany();
-  
+
   const categoriesMap = categoriesFromDB.reduce((map, category) => {
     map[category.name.toLocaleLowerCase()] = category.id;
     return map;
@@ -41,50 +39,44 @@ async function Main() {
 
   //insert products , productStock and Images
   products.forEach(async (product) => {
-
     let { images, type, stockPerType, ...rest } = product;
-
-    const mapSizes = Object.keys(stockPerType)
-    console.log(mapSizes);
-    
+    let mapSizes = Object.keys(stockPerType) as ValidSizes[];
 
     const productToInsert = {
       ...rest,
+      sizes: mapSizes,
       categoryId: categoriesMap[product.type],
     };
-    
+
     const productFromDb = await prisma.product.create({
       data: productToInsert,
     });
-    
+
     const dataToCreateImageInDb = images.map((image) => {
       return {
         productId: productFromDb.id,
         url: image,
       };
     });
-    
+
     await prisma.procutImage.createMany({
       data: dataToCreateImageInDb,
     });
 
-
-    const dataToCreateStockInDb = {...stockPerType, producStockId: productFromDb.id }
+    const dataToCreateStockInDb = {
+      ...stockPerType,
+      producStockId: productFromDb.id,
+    };
 
     // console.log("dataToCreateStockInDb",dataToCreateStockInDb);
-    
 
     await prisma.producStock.createMany({
-      data:dataToCreateStockInDb
-    })
-
-
+      data: dataToCreateStockInDb,
+    });
   });
- 
+
   await prisma.countrys.createMany({ data: countriesSeed });
-
 }
-
 
 (() => {
   Main();
--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -15,7 +15,7 @@ interface SeedProduct {
   // stockPerType: Partial<{ [key in ValidSizes]: number }>
 }
 
-type ValidSizes = "XS" | "S" | "M" | "L" | "XL" | "XXL" | "XXXL";
+export type ValidSizes = "XS" | "S" | "M" | "L" | "XL" | "XXL" | "XXXL";
 type ValidTypes = "shirts" | "pants" | "hoodies" | "hats";
 
 interface SeedData {
